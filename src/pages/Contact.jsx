980dbe9a71b4ef7740a57f3ffbcdbac1bcaa4a03
PerpetuumG <perpetuumg@gmail.com>
2023-11-25 14:44:05 +0200

    Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,84 @@
+import { useRef, useState } from 'react';
 
-const Contact= () => {
-	return (
-		<div>
-			Contact
-		</div>
-	);
+const Contact = () => {
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handeChange = e => {
+    e.preventDefault();
+  };
+
+  const handleFocus = () => {};
+
+  const handleBlur = () => {};
+
+  const handleSubmit = () => {};
+
+  return (
+    <section className={'relative flex lg:flex-row flex-col max-container'}>
+      <div className={'flex min-w-[50%] flex flex-col'}>
+        <h1 className={'head-text'}>Get in Touch </h1>
+
+        <form className={'w-full flex flex-col gap-7 mt-14'} onSubmit={handleSubmit}>
+          <label className={'text-black-500 font-semibold'}>
+            Name
+            <input
+              type='text'
+              name={'name'}
+              className={'input'}
+              placeholder={'John'}
+              required
+              value={form.name}
+              onChange={handeChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className={'text-black-500 font-semibold'}>
+            Email
+            <input
+              type='email'
+              name={'email'}
+              className={'input'}
+              placeholder={'john@gmail.com'}
+              required
+              value={form.email}
+              onChange={handeChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className={'text-black-500 font-semibold'}>
+            Your Message
+            <textarea
+              name={'message'}
+              rows={4}
+              className={'textarea'}
+              placeholder={"Let's me know hpw I can help you!"}
+              required
+              value={form.message}
+              onChange={handeChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type={'submit'}
+            className={'btn'}
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
