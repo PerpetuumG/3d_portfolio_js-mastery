7de8544de4796ae9a65046adb7675b2027107e2b
PerpetuumG <perpetuumg@gmail.com>
2023-11-26 10:50:12 +0200

    Alert

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,21 +4,25 @@ import { Canvas } from '@react-three/fiber';
 import Fox from '../models/Fox.jsx';
 import Loader from '../components/Loader.jsx';
 import useAlert from '../hooks/useAlert.js';
+import Alert from '../components/Alert.jsx';
 
 const Contact = () => {
-  const formRef = useRef(null);
+  const formRef = useRef();
   const [form, setForm] = useState({ name: '', email: '', message: '' });
-  const [isLoading, setIsLoading] = useState(false);
+  const { alert, showAlert, hideAlert } = useAlert();
+  const [loading, setLoading] = useState(false);
   const [currentAnimation, setCurrentAnimation] = useState('idle');
 
-  const { alert, showAlert, hideAlert } = useAlert();
-
-  const handeChange = e => {
-    setForm({ ...form, [e.target.name]: e.target.name });
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
   };
+
+  const handleFocus = () => setCurrentAnimation('walk');
+  const handleBlur = () => setCurrentAnimation('idle');
+
   const handleSubmit = e => {
     e.preventDefault();
-    setIsLoading(true);
+    setLoading(true);
     setCurrentAnimation('hit');
 
     emailjs
@@ -27,112 +31,129 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'Georg',
+          to_name: 'JavaScript Mastery',
           from_email: form.email,
-          to_email: '',
+          to_email: 'sujata@jsmastery.pro',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_ID,
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
       )
-      .then(() => {
-        setIsLoading(false);
-        showAlert({ show: true, text: 'Message sent successfully!', type: 'success' });
+      .then(
+        () => {
+          setLoading(false);
+          showAlert({
+            show: true,
+            text: 'Thank you for your message 😃',
+            type: 'success',
+          });
 
-        setTimeout(() => {
-          hideAlert();
+          setTimeout(() => {
+            hideAlert(false);
+            setCurrentAnimation('idle');
+            setForm({
+              name: '',
+              email: '',
+              message: '',
+            });
+          }, [3000]);
+        },
+        error => {
+          setLoading(false);
+          console.error(error);
           setCurrentAnimation('idle');
-          setForm({ name: '', email: '', message: '' });
-        }, 3000);
-      })
-      .catch(error => {
-        setIsLoading(false);
-        setCurrentAnimation('idle');
-        console.log(error);
-        showAlert({ show: true, text: "i didn't receive your message", type: 'danger' });
-      });
+
+          showAlert({
+            show: true,
+            text: "I didn't receive your message 😢",
+            type: 'danger',
+          });
+        },
+      );
   };
 
-  const handleFocus = () => setCurrentAnimation('walk');
-
-  const handleBlur = () => setCurrentAnimation('idle');
-
   return (
-    <section className={'relative flex lg:flex-row flex-col max-container'}>
+    <section className='relative flex lg:flex-row flex-col max-container'>
       {alert.show && <Alert {...alert} />}
 
-      <div className={'flex min-w-[50%] flex-col'}>
-        <h1 className={'head-text'}>Get in Touch </h1>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in Touch</h1>
 
-        <form className={'w-full flex flex-col gap-7 mt-14'} onSubmit={handleSubmit}>
-          <label className={'text-black-500 font-semibold'}>
+        <form ref={formRef} onSubmit={handleSubmit} className='w-full flex flex-col gap-7 mt-14'>
+          <label className='text-black-500 font-semibold'>
             Name
             <input
               type='text'
-              name={'name'}
-              className={'input'}
-              placeholder={'John'}
+              name='name'
+              className='input'
+              placeholder='John'
               required
               value={form.name}
-              onChange={handeChange}
+              onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
             />
           </label>
-
-          <label className={'text-black-500 font-semibold'}>
+          <label className='text-black-500 font-semibold'>
             Email
             <input
               type='email'
-              name={'email'}
-              className={'input'}
-              placeholder={'john@gmail.com'}
+              name='email'
+              className='input'
+              placeholder='John@gmail.com'
               required
               value={form.email}
-              onChange={handeChange}
+              onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
             />
           </label>
-
-          <label className={'text-black-500 font-semibold'}>
+          <label className='text-black-500 font-semibold'>
             Your Message
             <textarea
-              name={'message'}
-              rows={4}
-              className={'textarea'}
-              placeholder={"Let's me know hpw I can help you!"}
-              required
+              name='message'
+              rows='4'
+              className='textarea'
+              placeholder='Write your thoughts here...'
               value={form.message}
-              onChange={handeChange}
+              onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
             />
           </label>
 
           <button
-            type={'submit'}
-            className={'btn'}
-            disabled={isLoading}
+            type='submit'
+            disabled={loading}
+            className='btn'
             onFocus={handleFocus}
             onBlur={handleBlur}
           >
-            {isLoading ? 'Sending...' : 'Send Message'}
+            {loading ? 'Sending...' : 'Submit'}
           </button>
         </form>
       </div>
 
-      <div className={'lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'}>
-        <Canvas camera={{ position: [0, 0, 5], fov: 75, near: 0.1, far: 1000 }}>
-          <directionalLight intensity={2.5} position={[0, 0, 1]} />
-          <ambientLight intensity={0.5} />
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          <directionalLight position={[0, 0, 1]} intensity={2.5} />
+          <ambientLight intensity={1} />
+          <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} intensity={2} />
 
           <Suspense fallback={<Loader />}>
             <Fox
               currentAnimation={currentAnimation}
               position={[0.5, 0.35, 0]}
-              rotation={[12.6, -0.6, 0]}
+              rotation={[12.629, -0.6, 0]}
               scale={[0.5, 0.5, 0.5]}
-            ></Fox>
+            />
           </Suspense>
         </Canvas>
       </div>
